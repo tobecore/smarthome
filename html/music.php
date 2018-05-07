50a0dbdc99baa118dba3eaa9338d47a98295a253
tobecore <0004470@gmail.com>
2018-05-07 19:49:45 +0300

    added button

--- html/music.php
+++ html/music.php
@@ -1,6 +1,14 @@
 <?php
 
-echo "kdsjfhg";
 
+
+print_r($_GET[]);
+
+?>
+    <form action="music.php" method="get">
+        <input type="submit" name="start" value="Start">
+    </form>
+
+<?php
 
 ?>
