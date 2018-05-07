fc679a1f0cfb89b4e66a7c7930d545a16f81f929
tobecore <0004470@gmail.com>
2018-05-07 19:58:33 +0300

    added button

--- html/music.php
+++ html/music.php
@@ -3,10 +3,21 @@
 
 
 print_r($_GET);
+$action = $_GET['action'];
+
+switch ($action) {
+    case "Start":
+        exec("mplayer http://online-radioroks2.tavrmedia.ua/RadioROKS_NewRock");
+        break;
+    case "Pause":
+        break;
+}
+
 
 ?>
     <form action="music.php" method="get">
-        <input type="submit" name="start" value="Start">
+        <input type="submit" name="action" value="Start">
+        <input type="submit" name="action" value="Pause">
     </form>
 
 <?php
